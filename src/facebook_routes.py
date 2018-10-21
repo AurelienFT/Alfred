8f0c97664356c77d0c4912a632651d548a42c9b0
Aurélien Foucault <afoucault@localhost.localdomain>
2018-10-21 19:48:35 +0200

    add routes to test send message bot

--- src/facebook_routes.py
+++ src/facebook_routes.py
@@ -1,4 +1,4 @@
-from initialize import app, VERIFY_TOKEN
+from initialize import app, VERIFY_TOKEN, bot, random
 from flask import Flask, request
 
 @app.route("/facebook", methods=['GET', 'POST'])
@@ -6,6 +6,22 @@ def receive_message():
     if request.method == 'GET':
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
+    else:
+        # get whatever message a user sent the bot
+       output = request.get_json()
+       for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
+            if message.get('message'):
+                #Facebook Messenger ID for user so we know where to send response back to
+                recipient_id = message['sender']['id']
+                if message['message'].get('text'):
+                    response_sent_text = get_message()
+                    send_message(recipient_id, response_sent_text)
+                #if user sends us a GIF, photo,video, or any other non-text item
+                if message['message'].get('attachments'):
+                    response_sent_nontext = get_message()
+                    send_message(recipient_id, response_sent_nontext)
 
 @app.route("/ping", methods=['GET', 'POST'])
 def ping():
@@ -15,3 +31,14 @@ def verify_fb_token(token_sent):
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
+
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
+
+#uses PyMessenger to send response to user
+def send_message(recipient_id, response):
+    #sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
