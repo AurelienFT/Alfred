73cead8384b3b0dbe70759ab6209749dcdd6bc7e
root <root@vps585784.ovh.net>
2019-04-05 17:12:20 +0200

    Add initialize

--- src/initialize.py
+++ src/initialize.py
@@ -2,9 +2,9 @@ import random
 from flask import Flask, request
 from pymessenger.bot import Bot
 app = Flask(__name__)
-ACCESS_TOKEN = 'ACCESS_TOKEN'
-VERIFY_TOKEN = 'VERIFY_TOKEN'
-bot = Bot(ACCESS_TOKEN)
+FACEBOOK_ACCESS_TOKEN = 'EAAMoWPp4rwoBABiii4iDV8uSuZBl5MblHHtDhmbqw7lk93wwpirM53ZBeUWhseUWbxEsCsGGgpg2pfDUXuEhLL6LI3RWMBwm1IlFnP8B1q9ZC0QkEaWoAgCBn10g7no4VBlCyNJcVykHqImvZBwZClRe748ZA7ZCqh20qmkvrgvRwZDZD'
+VERIFY_TOKEN = 'income_messenger'
+bot = Bot(FACEBOOK_ACCESS_TOKEN)
 
 def initialize(alfred, platform):
     if platform == "facebook":
